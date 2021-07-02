cbe619401db8c6ccfc6d7fcedc7fcd36b4327047
Wang <lowsing3721@qq.com>
2021-07-02 16:03:54 +0800

    忽略fabfile

--- fabfile.py
+++ fabfile.py
@@ -9,5 +9,5 @@ def hello(name=''):
 def prepare_deploy():
     local("python manage.py test demo")
     # 前提已在当前目录git init
-    # local("git add -p && git commit")
+    local("git add . && git commit")
     # local("git push")
